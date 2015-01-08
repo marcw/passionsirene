5f212f942a8468097a94771720dc7463d1f35ee2
Marc Weistroff <marc@weistroff.net>
2015-01-08 10:20:27 +0100

    #JeSuisCharlie

--- passionsirene.rb
+++ passionsirene.rb
@@ -11,53 +11,52 @@ end
 
 r = Random.new
 
-original = "A" * r.rand(1..15)
-original += "H" * r.rand(4..15)
-if r.rand(0..60) == 10
-    original += "hipse"
-end
-original += "U" * r.rand(10..35)
+original = "CH"
+original += "A" * r.rand(8..35)
+#if r.rand(0..60) == 10
+    #original += "hipse"
+#end
+original += "RLIE #JeSuisCharlie"
 
+#def word_wrap(text, columns = 80)
+  #text.split("\n").collect do |line|
+    #line.length > columns ? line.gsub(/(.{1,#{columns}})(\s+|$)/, "\\1\n").strip : line
+  #end * "\n"
+#end
 
-def word_wrap(text, columns = 80)
-  text.split("\n").collect do |line|
-    line.length > columns ? line.gsub(/(.{1,#{columns}})(\s+|$)/, "\\1\n").strip : line
-  end * "\n"
-end
+#def chunk(string, size)
+    #string.scan(/.{1,#{size}}/)
+#end
 
-def chunk(string, size)
-    string.scan(/.{1,#{size}}/)
-end
+#colors = [
+    #["#fffd2f", "#000000"],
+    #["#0036ff", "#ffffff"],
+    #["#00ff32", "#ffffff"],
+    #["#f800ef", "#ffffff"],
+    #["#000000", "#ffffff"],
+#]
 
-colors = [
-    ["#fffd2f", "#000000"],
-    ["#0036ff", "#ffffff"],
-    ["#00ff32", "#ffffff"],
-    ["#f800ef", "#ffffff"],
-    ["#000000", "#ffffff"],
-]
+#color = colors.sample
 
-color = colors.sample
+#pointsize = 80
+#canvas = Magick::Image.new(435, 225) {self.background_color = color[0]}
+#gc = Magick::Draw.new
+#gc.font = 'impact.ttf'
+#gc.fill = color[1]
+#gc.stroke = color[1]
+#gc.text_antialias=true
+#gc.pointsize(pointsize)
 
-pointsize = 80
-canvas = Magick::Image.new(435, 225) {self.background_color = color[0]}
-gc = Magick::Draw.new
-gc.font = 'impact.ttf'
-gc.fill = color[1]
-gc.stroke = color[1]
-gc.text_antialias=true
-gc.pointsize(pointsize)
+#position = -5
+#chunk(original, 10).each do |row|
+  #gc.text(0, position += (pointsize- 15), row)
+#end
 
-position = -5
-chunk(original, 10).each do |row|
-  gc.text(0, position += (pointsize- 15), row)
-end
-
-gc.draw(canvas)
-begin
-    File.delete('tst.png')
-rescue
-end
-canvas.write('tst.png')
-Twitter.update_with_media(original, File.new('tst.png'))
-File.delete('tst.png')
+#gc.draw(canvas)
+#begin
+    #File.delete('tst.png')
+#rescue
+#end
+#canvas.write('tst.png')
+Twitter.update_with_media(original, File.new('charlie.jpg'))
+#File.delete('tst.png')
